8a191a7093b71fc791752923a324fc865f1014ce
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-15 19:56:56 -0700

    Add basic xUnit project

---
+++ PoshMailKitTest/SendMKMailMessageTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PoshMailKitTest
+{
+    public class SendMKMailMessageTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
