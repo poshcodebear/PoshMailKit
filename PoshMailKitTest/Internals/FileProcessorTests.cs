b04a823f5f590c4b949992ef711f57266f8c969b
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-17 12:33:38 -0700

    Add the rest of the FileProcessor tests

--- PoshMailKitTest/Internals/FileProcessorTests.cs
+++ PoshMailKitTest/Internals/FileProcessorTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using PoshMailKit.Internals;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
@@ -18,7 +19,6 @@ namespace PoshMailKitTest.Internals
         public FileProcessorTests()
         {
             mockRepository = new MockRepository(MockBehavior.Strict);
-
             mockFileSystem = mockRepository.Create<IFileSystem>();
         }
 
@@ -33,39 +33,81 @@ namespace PoshMailKitTest.Internals
         }
 
         [Fact]
-        public void GetFullPathName_StateUnderTest_ExpectedBehavior()
+        public void GetFullPathName_FileNameOnly_ReturnsFullPath()
         {
             // Arrange
-            var fileProcessor = CreateFileProcessor(@"", GetMockFileSystem());
-            string fileName = null;
+            var fileName = @"C:\test\tmp\test.txt";
+            var filePath = @"C:\test\tmp";
+            var expectedReturnPath = @"C:\test\tmp\test.txt";
+
+            var fileProcessor = CreateFileProcessor(filePath, GetMockFileSystem());
 
             // Act
-            var result = fileProcessor.GetFullPathName(
-                fileName);
+            var result = fileProcessor.GetFullPathName(fileName);
 
             // Assert
-            Assert.True(false);
+            Assert.True(result == expectedReturnPath,
+                $"Expected: {expectedReturnPath}, actual: {result}");
             mockRepository.VerifyAll();
         }
 
         [Fact]
-        public void GetFileMimePart_SimpleFileAttachment_ReturnsCorrectMimePart()
+        public void GetFullPathName_FileRelativePath_ReturnsFullPath()
+        {
+            // Arrange
+            var fileName = @"..\test.txt";
+            var filePath = @"C:\test\tmp";
+            var expectedReturnPath = @"C:\test\test.txt";
+
+            var fileProcessor = CreateFileProcessor(filePath, GetMockFileSystem());
+
+            // Act
+            var result = fileProcessor.GetFullPathName(fileName);
+
+            // Assert
+            Assert.True(result == expectedReturnPath,
+                $"Expected: {expectedReturnPath}, actual: {result}");
+            mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void GetFullPathName_FileFullPath_ReturnsFullPath()
+        {
+            // Arrange
+            var fileName = "test.txt";
+            var filePath = @"C:\test\tmp";
+            var expectedReturnPath = @"C:\test\tmp\test.txt";
+
+            var fileProcessor = CreateFileProcessor(filePath, GetMockFileSystem());
+
+            // Act
+            var result = fileProcessor.GetFullPathName(fileName);
+
+            // Assert
+            Assert.True(result == expectedReturnPath,
+                $"Expected: {expectedReturnPath}, actual: {result}");
+            mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void GetFileMimePart_WithoutLabel_ReturnsCorrectMimePart()
         {
             // Arrange
             var fileName = @"test.txt";
             var filePath = @"C:\test";
             var fileContents = "This is a test text file";
+            var contentDispositionType = ContentDispositionType.Attachment;
             var expectedContentType = new ContentType("text", "plain");
-            var expectedContentDisposition = new ContentDisposition(ContentDisposition.Attachment);
+            var expectedContentDisposition = new ContentDisposition(contentDispositionType.ToString());
 
             var mockFiles = new Dictionary<string, MockFileData>()
             {
-                { $"{filePath}\\{fileName}", new MockFileData("This is a test text file") },
+                { $"{filePath}\\{fileName}", new MockFileData(fileContents) },
             };
             var fileProcessor = CreateFileProcessor(filePath, new MockFileSystem(mockFiles));
 
             // Act
-            var result = fileProcessor.GetFileMimePart(fileName);
+            var result = fileProcessor.GetFileMimePart(fileName, contentDispositionType);
 
             // Assert
             var stream = new System.IO.MemoryStream();
@@ -74,34 +116,54 @@ namespace PoshMailKitTest.Internals
             result.Content.DecodeTo(stream);
             string actualContent = encoding.GetString(stream.ToArray());
 
-            Assert.True(actualContent == fileContents,
+            Assert.True(result.FileName == fileName,
                 $"Filename: expected '{fileName}', actual '{result.FileName}'");
-            Assert.True(actualContent == fileContents,
-                $"ContentDisposition: expected '{expectedContentDisposition.Disposition}', actual '{result.ContentDisposition.Disposition}'");
+            Assert.True(result.ContentDisposition.Disposition == expectedContentDisposition.Disposition,
+                $"ContentDispositionType: expected '{expectedContentDisposition.Disposition}', actual '{result.ContentDisposition.Disposition}'");
             Assert.True(actualContent == fileContents,
                 $"File contents: expected '{fileContents}', actual '{actualContent}'");
-            Assert.True(actualContent == fileContents,
+            Assert.True(result.ContentType.MimeType == expectedContentType.MimeType,
                 $"ContentType: expected '{expectedContentType.MimeType}', actual '{result.ContentType.MimeType}'");
             mockRepository.VerifyAll();
         }
 
         [Fact]
-        public void GetFileMimePart_StateUnderTest_ExpectedBehavior1()
+        public void GetFileMimePart_WithLabel_ReturnsCorrectMimePart()
         {
             // Arrange
-            var fileProcessor = CreateFileProcessor(@"", GetMockFileSystem());
-            string fileName = null;
-            ContentDisposition contentDisposition = null;
-            string label = null;
+            var fileName = @"test.png";
+            var filePath = @"C:\test";
+            var fileLabel = "test-label";
+            var fileContents = new byte[] { 0xf3, 0xab, 0x12, 0xd7};
+            var contentDispositionType = ContentDispositionType.Inline;
+            var expectedContentType = new ContentType("image", "png");
+            var expectedContentDisposition = new ContentDisposition(contentDispositionType.ToString());
+
+            var mockFiles = new Dictionary<string, MockFileData>()
+            {
+                { $"{filePath}\\{fileName}", new MockFileData(fileContents) },
+            };
+            var fileProcessor = CreateFileProcessor(filePath, new MockFileSystem(mockFiles));
 
             // Act
-            var result = fileProcessor.GetFileMimePart(
-                fileName,
-                contentDisposition,
-                label);
+            var result = fileProcessor.GetFileMimePart(fileName, contentDispositionType, fileLabel);
 
             // Assert
-            Assert.True(false);
+            var stream = new System.IO.MemoryStream();
+
+            result.Content.DecodeTo(stream);
+            var actualContent = stream.ToArray();
+
+            Assert.True(result.FileName == fileName,
+                $"Filename: expected '{fileName}', actual '{result.FileName}'");
+            Assert.True(result.ContentId == fileLabel,
+                $"Filename: expected '{fileLabel}', actual '{result.ContentId}'");
+            Assert.True(result.ContentDisposition.Disposition == expectedContentDisposition.Disposition,
+                $"ContentDispositionType: expected '{expectedContentDisposition.Disposition}', actual '{result.ContentDisposition.Disposition}'");
+            Assert.True(Enumerable.SequenceEqual(actualContent, fileContents),
+                $"File contents: expected '{BitConverter.ToString(fileContents)}', actual '{BitConverter.ToString(actualContent)}'");
+            Assert.True(result.ContentType.MimeType == expectedContentType.MimeType,
+                $"ContentType: expected '{expectedContentType.MimeType}', actual '{result.ContentType.MimeType}'");
             mockRepository.VerifyAll();
         }
     }
