5d7a5ec553de0b1817a36d0106050e1c5c8a9a68
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-17 17:54:18 -0700

    Add test boilerplate for MailMessage

---
+++ PoshMailKitTest/Internals/MailMessageTests.cs
@@ -0,0 +1,67 @@
+﻿using MimeKit;
+using MimeKit.Text;
+using Moq;
+using PoshMailKit.Internals;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PoshMailKitTest.Internals
+{
+    public class MailMessageTests
+    {
+        private MockRepository mockRepository;
+
+
+
+        public MailMessageTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        private MailMessage CreateMailMessage()
+        {
+            return new MailMessage();
+        }
+
+        [Fact]
+        public void AddAttachments_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var mailMessage = this.CreateMailMessage();
+            List<MimePart> filesToAttach = null;
+
+            // Act
+            mailMessage.AddAttachments(
+                filesToAttach);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void NewMailBody_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var mailMessage = this.CreateMailMessage();
+            TextFormat format = default(global::MimeKit.Text.TextFormat);
+            System.Text.Encoding charsetEncoding = null;
+            string body = null;
+            ContentEncoding contentTransferEncoding = default(global::MimeKit.ContentEncoding);
+
+            // Act
+            mailMessage.NewMailBody(
+                format,
+                charsetEncoding,
+                body,
+                contentTransferEncoding);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
