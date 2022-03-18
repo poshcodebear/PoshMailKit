3cfed2bcfccecb1d1fcdd56253ce07c16a8cc0a7
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-18 15:51:43 -0700

    Remove unused usings

--- PoshMailKit/Internals/SmtpProcessor.cs
+++ PoshMailKit/Internals/SmtpProcessor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MimeKit;
-using MailKit.Net.Smtp;
+﻿using MimeKit;
 using MailKit.Security;
 using MailKit;
 
