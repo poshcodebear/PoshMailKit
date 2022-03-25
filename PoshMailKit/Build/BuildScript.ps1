5dcaa56f234b13e2825b5a2c0710e55873a45b81
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-25 01:06:45 -0700

    Add manifest templating

--- PoshMailKit/Build/BuildScript.ps1
+++ PoshMailKit/Build/BuildScript.ps1
@@ -1,8 +1,11 @@
-﻿$sourceFolder = "$($PSScriptRoot)\.."
+﻿# Config
+$sourceFolder = "$($PSScriptRoot)\.."
 $conf = Get-Content -Path "$($PSScriptRoot)\BuildConfig.json" | ConvertFrom-Json
 
 $modFolder = "$($sourceFolder)\$($conf.Name)"
+$modManifest = "$($modFolder)\$($conf.Name).psd1"
 
+# Module population
 if (Test-Path -Path $modFolder)
 {
     Remove-Item -Path $modFolder -Force -Recurse
@@ -16,6 +19,19 @@ foreach ($file in $conf.FileList)
 
 Copy-Item -Path "$($sourceFolder)\ModuleMembers\*" -Destination $modFolder -Recurse
 
+# Manifest templating
+$prTag = $conf.PreReleaseTag ? "Prerelease = '$($conf.PreReleaseTag)'" : "# Prerelease = ''"
+
+$manifest = Get-Content -Path $modManifest -Raw
+$manifest = $manifest.Replace('<#$Version#>', $conf.Version).
+                      Replace('<#$PreRelease#>', $prTag).
+                      Replace('<#$Date#>', (Get-Date -Format 'yyyy-MM-dd')).
+                      Replace('<#$Year#>', (Get-Date -Format 'yyyy')).
+                      Replace('<#$FileList#>', "'$($conf.FileList -join "', '")'")
+
+$manifest | Out-File -FilePath $modManifest -Force
+
+# Module archive (zip) generation
 $archiveFilename = "$($conf.Name)-$($conf.Version)"
 if ($conf.PreReleaseTag)
 {
