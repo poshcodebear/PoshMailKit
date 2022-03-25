89fd3567d6c286931e854e245dd6fb273f8f942c
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-25 00:44:39 -0700

    Add auto-archive

--- PoshMailKit/Build/BuildScript.ps1
+++ PoshMailKit/Build/BuildScript.ps1
@@ -1,8 +1,7 @@
-﻿$module = 'PoshMailKit'
-$sourceFolder = "$($PSScriptRoot)\.."
-$modFolder = "$($sourceFolder)\$($module)"
-
+﻿$sourceFolder = "$($PSScriptRoot)\.."
 $conf = Get-Content -Path "$($PSScriptRoot)\BuildConfig.json" | ConvertFrom-Json
+
+$modFolder = "$($sourceFolder)\$($conf.Name)"
 
 if (Test-Path -Path $modFolder)
 {
@@ -16,3 +15,18 @@ foreach ($file in $conf.FileList)
 }
 
 Copy-Item -Path "$($sourceFolder)\ModuleMembers\*" -Destination $modFolder -Recurse
+
+$archiveFilename = "$($conf.Name)-$($conf.Version)"
+if ($conf.PreReleaseTag)
+{
+    $archiveFilename += "-$($conf.PreReleaseTag)"
+}
+
+$compressArchiveSplat = @{
+    Path              = "$($modFolder)\*"
+    DestinationPath   = "$($sourceFolder)\$($archiveFilename).zip"
+    CompressionLevel  = 'Optimal'
+    Force             = $true
+}
+
+Compress-Archive @compressArchiveSplat
