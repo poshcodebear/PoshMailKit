d12b97cd3975aae1218dd2c96055c2463a7823e1
The PowerShell Bear <poshcodebear@outlook.com>
2022-03-25 00:14:46 -0700

    Add basic build script

---
+++ PoshMailKit/Build/BuildScript.ps1
@@ -0,0 +1,18 @@
+﻿$module = 'PoshMailKit'
+$sourceFolder = "$($PSScriptRoot)\.."
+$modFolder = "$($sourceFolder)\$($module)"
+
+$conf = Get-Content -Path "$($PSScriptRoot)\BuildConfig.json" | ConvertFrom-Json
+
+if (Test-Path -Path $modFolder)
+{
+    Remove-Item -Path $modFolder -Force -Recurse
+}
+New-Item -Path $modFolder -ItemType Directory
+
+foreach ($file in $conf.FileList)
+{
+    Copy-Item -Path "$($sourceFolder)\$($file)" -Destination "$($modFolder)"
+}
+
+Copy-Item -Path "$($sourceFolder)\ModuleMembers\*" -Destination $modFolder -Recurse
