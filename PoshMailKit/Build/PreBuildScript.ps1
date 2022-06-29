2a5617486dec5bcb7411a635dc46aa4b93af9c88
The PowerShell Bear <poshcodebear@outlook.com>
2022-06-29 15:49:50 -0700

    Add prebuild script to update project version

---
+++ PoshMailKit/Build/PreBuildScript.ps1
@@ -0,0 +1,17 @@
+﻿# Config
+$sourceFolder = "$($PSScriptRoot)\.."
+$conf = Get-Content -Path "$($PSScriptRoot)\BuildConfig.json" | ConvertFrom-Json
+
+$projectFile = "$($sourceFolder)\PoshMailKit.csproj"
+
+# Project file version update
+$prTag = $conf.PreReleaseTag ? "-$($conf.PreReleaseTag)" : ''
+
+$projectContents = Get-Content -Path $projectFile -Raw
+
+$projectContents = $projectContents -replace '<VersionPrefix>[^<]*</VersionPrefix>', "<VersionPrefix>$($conf.Version)</VersionPrefix>"
+$projectContents = $projectContents -replace '<VersionSuffix>[^<]*</VersionSuffix>', "<VersionSuffix>$($prTag)</VersionSuffix>"
+
+$projectContents = $projectContents.TrimEnd("`r`n")
+
+$projectContents | Out-File -FilePath $projectFile -Force
